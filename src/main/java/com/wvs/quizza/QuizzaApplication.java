6b31b14acc481c2fe2610d749941116e3defb96d
Martin Beyer <martin.beyer@live.de>
2019-01-15 12:02:29 +0100

    inital commit

---
+++ src/main/java/com/wvs/quizza/QuizzaApplication.java
@@ -0,0 +1,14 @@
+package com.wvs.quizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/wvs/quizza/QuizzaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wvs.quizza;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuizzaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
